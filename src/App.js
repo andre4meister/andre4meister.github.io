f4ef84e5515630b0463c0173de5eb51ba3a0d091
andrii <darkness1237893@gmail.com>
2022-03-08 22:30:36 +0200

    21th,20th,19th lesson`s, navlink, new components, activeClassName(doesn`t work) and dialigs css nad html

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Nav from './components/Navbar/Nav';
 import Dialogs from './components/Dialogs/Dialogs'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Music from './components/Music/Music';
+import News from './components/News/News';
+import Settings from './components/Settingss/Settings';
 
 function App() {
   return (
-    <div className='app-wrapper'>
-      <Header/>
-      <Nav/>
-      <div className='app-wrapper-content'>
-        <Dialogs/>
-        {/* <Profile/> */}
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Nav />
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+          </Routes>
+        </div>
       </div>
-      
-    </div>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import React from 'react'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Andrii
+                </div>
+                <div className={s.dialog}>
+                    Dima
+                </div>
+                <div className={s.dialog}>
+                    Marik
+                </div>
+                <div className={s.dialog}>
+                    Vova
+                </div>
+                <div className={s.dialog}>
+                    Roman
+                </div>
+                <div className={s.dialog}>
+                    Bohdan
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your dog?</div>
+                <div className={s.message}>Privet</div>
+            </div>
         </div>
     )
 }
